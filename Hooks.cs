1156c8f307064f080f9635cf0808fde6862adadc
deepak <anandhadeepak@gmail.com>
2014-08-19 12:28:30 -0700

    added hooks

---
+++ Hooks.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowSample
+{
+    [Binding]
+    class Hooks
+    {
+        private static FirefoxDriver driver;
+        [BeforeTestRun]
+        public static void StartSelenium()
+        {
+            driver  = new FirefoxDriver();
+        }
+
+        [AfterTestRun]
+        public static void Close()
+        {
+            driver.Quit();
+        }
+
+    }
+}
